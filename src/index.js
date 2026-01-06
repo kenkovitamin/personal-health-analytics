673a3da95647b834810429435cf135b37998af16
kenkovitamin <kenko.vitamin@gmail.com>
2026-01-06 14:29:17 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ status: "API running" });
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log("Server started on port", PORT);
+});
